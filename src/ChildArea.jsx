44644e9e336e1bdf2d659db96f0a6807a86d7b68
reiko51 <71922155+reiko51@users.noreply.github.com>
2022-07-12 15:56:55 +0900

    レンダリング最適化（１）

--- src/ChildArea.jsx
+++ src/ChildArea.jsx
@@ -1,11 +1,21 @@
+import { memo } from "react";
+
 const style = {
   width: "100%",
   height: "200px",
   backgroundColor: "khaki"
 };
-
-export const ChildArea = (props) => {
+// memo:propsが変わらない限り再レンダリングしない
+// 複数の要素で成り立つ規模のコンポーネントは、基本メモで囲う
+export const ChildArea = memo((props) => {
   const { open } = props;
+  console.log("チルドレンエリアがレンダリングされた！");
+
+  const data = [...Array(2000).keys()];
+
+  data.forEach(() => {
+    console.log("---");
+  });
   return (
     <>
       {open ? (
@@ -15,4 +25,4 @@ export const ChildArea = (props) => {
       ) : null}
     </>
   );
-};
+});
