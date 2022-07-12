e9a170ddfd3bae69052c2b80532ea5a0418e5806
reiko51 <71922155+reiko51@users.noreply.github.com>
2022-07-12 16:15:37 +0900

    レンダリングの最適化

--- src/App.js
+++ src/App.js
@@ -1,22 +1,25 @@
-import { useState } from "react";
+import { useCallback, useState } from "react";
 import { ChildArea } from "./ChildArea";
 import "./styles.css";
 
 export default function App() {
   const [text, setText] = useState("");
   const [open, setOpen] = useState(false);
-  // stateの値が変わることで再レンダリングされる
+
   const onChangeText = (e) => setText(e.target.value);
-  const onClickOpen = () => {
-    setOpen(!open);
-  };
+
+  const onClickOpen = () => setOpen(!open);
+  // 関数はレンダリングの都度作成されるので
+  // 関数をメモ化して必要なときだけ走るようにする
+  const onClickClose = useCallback(() => setOpen(false), []);
+
   return (
     <div className="App">
       <input value={text} onChange={onChangeText} />
       <br />
       <br />
       <button onClick={onClickOpen}>表示</button>
-      <ChildArea open={open} />
+      <ChildArea open={open} onClickClose={onClickClose} />
     </div>
   );
 }
